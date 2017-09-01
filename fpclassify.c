5898d700b03ee45572419070cffbf5f611d3aa4e
bel2125 <bel2125@gmail.com>
2017-09-01 10:49:50 +0200

    Add fpclassify for double values

---
+++ fpclassify.c
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 bel2125
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#include "fpclassify.h"
+
+double DOUBLE_ZERO = 0.0;
+
+#if defined(NEED_fpclassify)
+
+#if defined(HAVE_STDINT_H)
+#include <stdint.h>
+typedef uint64_t bitfield64;
+#elif defined(_MSC_VER)
+typedef unsigned __int64 bitfield64;
+#else
+typedef unsigned long long bitfield64;
+#endif
+
+static bitfield64 ms = 0x8000000000000000;
+static bitfield64 me = 0x7FF0000000000000;
+static bitfield64 mf = 0x000FFFFFFFFFFFFF;
+
+int fpclassify(double d) 
+{
+  bitfield64 *p = (bitfield64 *)&d;
+  if ((*p & me) != me) {
+    /* finite */
+    if (*p & mf) {
+      /* finite and not null */
+      if (*p & me) {
+        return FP_NORMAL;
+      }
+      return FP_SUBNORMAL;
+    }	
+    return FP_ZERO;
+  }
+  if (*p & mf) {
+    return FP_NAN;
+  }
+  return FP_INFINITE;
+}
+
+#endif
+
---
+++ fpclassify.h
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 bel2125
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef FPCLASSIFY_H
+#define FPCLASSIFY_H
+
+/* define NAN, INFINITY, isnan(), isinf(), isfinite() */
+#include <math.h>
+
+/* However, some older Visual Studio Versions do not */
+#if !defined(INFINITY) || !defined(NAN)
+extern double DOUBLE_ZERO;
+#define INFINITY (1.0/DOUBLE_ZERO)
+#define NAN (DOUBLE_ZERO/DOUBLE_ZERO)
+#endif
+#if !defined(isnan) || !defined(isinf) || !defined(isfinite)
+#define NEED_fpclassify
+extern int fpclassify(double d);
+#define FP_INFINITE (1)
+#define FP_NAN (2)
+#define FP_ZERO (4)
+#define FP_NORMAL (8)
+#define FP_SUBNORMAL (16)
+#define isnan(x) ((fpclassify((double)(x)) & FP_NAN) == FP_NAN)
+#define isinf(x) ((fpclassify((double)(x)) & FP_INFINITE) == FP_INFINITE)
+#define isfinite(x) ((fpclassify((double)(x)) & (FP_NAN|FP_INFINITE)) == 0)
+#endif
+
+#endif /* FPCLASSIFY_H */
+
